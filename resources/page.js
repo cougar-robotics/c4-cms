5bdbc5f644570ae8fff4c06c6fb08524004a9220
Casey <casey@Biscuits.(none)>
2012-10-27 10:43:21 -0400

    Switch to express + mongoose over flatiron

--- resources/page.js
+++
@@ -1,9 +0,0 @@
-var resourceful = require('resourceful');
-var document = require('./document');
-var config = require('../config');
-var _ = require('lodash');
-
-var Page = module.exports = resourceful.define('page', function() {
-    document.schema(this);
-    this.child('page');
-});
