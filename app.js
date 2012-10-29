b5f40764274513be100507f1b88efc0572118f54
Michael R <michael.r1797@gmail.com>
2012-10-29 12:17:44 -0400

    Added a support route.
    Casey, fix the app.js! I wan't to see it in action!

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/support', function(req, res){
+  res.render('home', { title: 'Support'});
+});
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
